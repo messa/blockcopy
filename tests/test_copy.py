e6132979f1b7ad873d3cd8591aa9fc4399bec9fe
Petr Messner <petr@leadhub.co>
2025-03-06 21:54:44 +0000

    add some tests

--- tests/test_copy.py
+++ tests/test_copy.py
@@ -35,3 +35,63 @@ def test_copy(tmp_path, script_path):
         assert p3.wait() == 0
 
     assert dst_path.read_bytes() == test_content
+
+
+def test_copy_to_larger_device(tmp_path, script_path):
+    '''
+    This tests simulates copying from smaller to larger device or file.
+    Contents of the destination file is replaced with the source file contents.
+    Contents of the destination file beyond the size of the source file is not changed.
+    '''
+    test_content = b'Test content.' * 1024000
+    src_path = tmp_path / 'src_file'
+    src_path.write_bytes(test_content)
+    dst_path = tmp_path / 'dst_file'
+    dst_path.write_bytes(b'-' * len(test_content) + b'abc')
+
+    cmd1 = [script_path, 'checksum', str(dst_path)]
+    cmd2 = [script_path, 'retrieve', str(src_path)]
+    cmd3 = [script_path, 'save', str(dst_path)]
+
+    with ExitStack() as stack:
+        p1 = stack.enter_context(Popen(cmd1, stdin=DEVNULL, stdout=PIPE))
+        p2 = stack.enter_context(Popen(cmd2, stdin=p1.stdout, stdout=PIPE))
+        p3 = stack.enter_context(Popen(cmd3, stdin=p2.stdout))
+        assert p1.wait() == 0
+        assert p2.wait() == 0
+        assert p3.wait() == 0
+
+    assert dst_path.read_bytes() == test_content + b'abc'
+
+
+def test_copy_identical(tmp_path, script_path):
+    '''
+    This tests checks that no actual data is copied when source and destination are identical.
+    '''
+    test_content = b'Test content.' * 1024000
+    src_path = tmp_path / 'src_file'
+    src_path.write_bytes(test_content)
+    dst_path = tmp_path / 'dst_file'
+    dst_path.write_bytes(test_content)
+
+    cmd1 = [script_path, 'checksum', str(dst_path)]
+    cmd2 = [script_path, 'retrieve', str(src_path)]
+    cmd3 = [script_path, 'save', str(dst_path)]
+
+    with ExitStack() as stack:
+        p1 = stack.enter_context(Popen(cmd1, stdin=DEVNULL, stdout=PIPE))
+        p2 = stack.enter_context(Popen(cmd2, stdin=p1.stdout, stdout=PIPE))
+        p3 = stack.enter_context(Popen(cmd3, stdin=PIPE))
+
+        p2_output = p2.stdout.read()
+        p3.stdin.write(p2_output)
+        p3.stdin.flush()
+
+        assert p1.wait() == 0
+        assert p2.wait() == 0
+        assert p3.wait() == 0
+
+        # retrieve output should be trivial
+        assert p2_output == b'done'
+
+    assert dst_path.read_bytes() == test_content
